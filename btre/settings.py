58235be23a798745e6f2357f3936984aacab1086
Proskurenko Danylo <proskurenko.danil0@gmail.com>
2020-10-26 08:51:31 +0300

    search made

--- btre/settings.py
+++ btre/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
